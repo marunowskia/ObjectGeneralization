8ce701d4c502c54979e99c35b843e3c90fb75c3e
Alexander Marunowski <marunowskia@gmail.com>
2017-02-17 22:12:20 -0800

    added genericization of generated interface. fixed location of output file.

--- src/main/java/com/github/marunowskia/interfacegenerator/InterfaceComposer.java
+++ src/main/java/com/github/marunowskia/interfacegenerator/InterfaceComposer.java
@@ -5,20 +5,25 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.google.common.io.Files;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class InterfaceComposer {
 
 	public static List<InterfaceDefinition> composeInterfaces() {
 		return null;
 	}
-	
+
 	public static void outputInterfaces(List<InterfaceDefinition> requiredInterfaces, File parentDirectory) {
 		requiredInterfaces.forEach(def -> {
 			StringBuilder builder = new StringBuilder();
-			
+
 			// ==================== Output file structure ====================
 			// package PKG;
 			// import *.*;
@@ -26,31 +31,46 @@ public class InterfaceComposer {
 			// 		public <? extends GenericType> ReturnType getSomething();
 			// 		...
 			// }
-			
-			
+
+
 			List<String> extendsList = def.mustExtend.stream().map(id->id.name).collect(Collectors.toList());
 
 			builder.append("package ").append(def.pkg).append(";\n\n");
 			def.dependencies.forEach(str -> builder.append("import ").append(str).append(";\n"));
-			builder.append("public interface ").append(def.name).append(" ").append(String.join(", ", extendsList)).append(" {\n");
-			def.methodSignatures.forEach(str -> builder.append(str).append(";\n"));
+			builder.append("\n\npublic interface ").append(def.name);
+
+			if(0 < Optional.ofNullable(def.genericParameters).map(List::size).orElse(0)) {
+				System.out.println("have generic parameters");
+				builder.append(" <").append(String.join(", ", def.genericParameters)).append("> ");
+			}
+
+			builder.append(" ").append(String.join(", ", extendsList)).append(" {\n");
+			def.methodSignatures.forEach(str -> builder.append("\t").append(str).append(";\n"));
 			builder.append('}');
-			
-			
+
+
 			// Write the string builder's content to the appropriate output file.
 			try {
-				Path outputPath = Paths.get(parentDirectory.getAbsolutePath(), def.pkg.split("\\."));
+				Path outputPath = Paths.get(parentDirectory.getAbsolutePath(), def.pkg.split("\\.")).resolve(def.name);
+				// SLF4J just stopped working?
+				System.out.println(outputPath);
 				File outputFile = outputPath.toFile();
+				outputFile.delete();
+				Files.createParentDirs(outputFile);
+				outputFile.createNewFile();
 				Files.write(builder.toString().getBytes(), outputFile);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		});
 	}
-	
+
+	@Data
 	public static class InterfaceDefinition {
+
 		public String pkg;
 		public String name;
+		public List<String> genericParameters;
 		public List<String> dependencies;
 		public List<InterfaceDefinition> mustExtend;
 		public List<String> methodSignatures;
