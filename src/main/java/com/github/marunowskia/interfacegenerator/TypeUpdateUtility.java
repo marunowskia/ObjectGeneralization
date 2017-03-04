057feb636dd6460b83dff89b65628e8a81e9bcb8
Alexander Marunowski <marunowskia@gmail.com>
2017-03-03 20:53:12 -0800

    replaced horrible type-update mechanism with a proper parse tree

--- src/main/java/com/github/marunowskia/interfacegenerator/TypeUpdateUtility.java
+++ src/main/java/com/github/marunowskia/interfacegenerator/TypeUpdateUtility.java
@@ -1,137 +1,79 @@
 package com.github.marunowskia.interfacegenerator;
-
-
+import static org.apache.commons.lang3.StringUtils.*;
 import static com.google.common.base.Preconditions.*;
+import static java.util.Optional.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.github.marunowskia.interfacegenerator.InterfaceComposer.InterfaceDefinition;
-import com.google.common.base.Preconditions;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class TypeUpdateUtility {
-	private static final Logger log = LoggerFactory.getLogger(TypeUpdateUtility.class);
-	
-	
-//	public static <? extends Object> test() {return null};
-	public static String updateType(String oldType, Hashtable<String, InterfaceDefinition> replacements) {
-		checkArgument(bracketsAreBalanced(oldType));
-		checkArgument(!StringUtils.isEmpty(StringUtils.substringBefore(oldType, "<")));
-		// Every non-keyword and non-symbol in the type-string needs to be replaced with the interface version of the same type.
-		// Genericizaction parameters need to be replaced with their lower-bounded wildcard alternatives ("read only generic")
+
+	public static String updateType(String type, Hashtable<String, InterfaceDefinition> replacements) {
+		checkNotNull(type, "type not specified");
+		type = type.trim();
+		checkArgument(Character.isJavaIdentifierStart(type.charAt(0)), "Type is not a valid java type.");
+		checkArgument(checkBalancedBrackets(type), "Brackets are not balanced");
 		
-		// XXX: Remove comments strings from type definitions
+		// FIXME: There are invalid java type string that can get past these input checks.
 		
-		StringBuilder updatedTypeBuilder = new StringBuilder();
-		updatedTypeBuilder.append(updateOuterTypeComponent(StringUtils.substringBefore(oldType, "<"), replacements));
+		String outerComponent = substringBefore(type, "<");
+		outerComponent = ofNullable(replacements.get(outerComponent.trim())).map(def -> def.getName()).orElse(outerComponent);
 		
-		if(oldType.matches(".+<.*>")) {
-			updatedTypeBuilder.append(updateInnerTypeComponent("<"+StringUtils.substringAfter(oldType, "<"), replacements));
+		StringBuilder resultBuilder = new StringBuilder();
+		resultBuilder.append(outerComponent);
+		
+		if(type.matches(".+<.*>")) {
+			String innerComponent =  substringBeforeLast(substringAfter(type, "<"), ">");
+			innerComponent = updateGenericList(innerComponent, replacements);
+			resultBuilder	.append('<')
+							.append(innerComponent)
+							.append('>');
 		}
 		
-		return updatedTypeBuilder.toString();  
+		return normalizeSpace(resultBuilder.toString());
 	}
 	
-	/**
-	 * Inner type 
-	 * @param oldType. Always starts with < and ends with >
-	 * @param replacements
-	 * @return
-	 */
-	private static String updateInnerTypeComponent(String oldType, Hashtable<String, InterfaceDefinition> replacements) {
-		// Example: oldType = "<T0, ? extends T1<T2 extends T3>>"
+	public static String updateGenericList(String genericList, Hashtable<String, InterfaceDefinition> replacements) {
+		List<String> updatedGenerics = new ArrayList<>();
 		
-		
-		String bracketsRemoved = StringUtils.substringBeforeLast(StringUtils.substringAfter(oldType, "<"), ">");
-		// Example: bracketsRemoved = "T0, ? extends T1<T2 extends T3>"
-		
-		
-		if(oldType.matches(".*<.*>.*")) {
-			checkArgument(bracketsAreBalanced(oldType));
-			
-			String subType = StringUtils.substringAfter(bracketsRemoved, "<");
-			String updatedSubTypes = Arrays.stream(subType.split(","))
-					.map(s->s.trim())
-					.map(s->updateInnerTypeComponent(s, replacements))
-					.reduce("", (result, data) -> result + ", " + data);
-			updatedSubTypes.replaceFirst(",", "");
-			
-			return
-				"<"
-			+	updateFirstInnerType(StringUtils.substringBefore(bracketsRemoved, "<"), replacements) // UPDATE THE TYPE BEING GENERICIZED
-			+	(StringUtils.isEmpty(subType) ? "":updateInnerTypeComponent("<"+StringUtils.substringAfter(bracketsRemoved, "<"), replacements)) // UPDATE THE TYPE OF THE GENERIC COMPONENTS
-			+	">";	
+		StringBuilder genericTypeBuffer = new StringBuilder();
+		int depth = 0;
+		for(char c : genericList.toCharArray()) {
+			if(c == '<') depth++;
+			if(c == '>') depth--;
+			if(c == ',' && depth == 0) {
+				updatedGenerics.add(updateGeneric(genericTypeBuffer.toString(), replacements));
+				genericTypeBuffer.setLength(0);
+			}
+			else {
+				genericTypeBuffer.append(c);
+			}
 		}
-		else {
-			
-		}
-		
-		return
-				updateFirstInnerType(StringUtils.substringBefore(bracketsRemoved, "<"), replacements);
+		updatedGenerics.add(updateGeneric(genericTypeBuffer.toString(), replacements));
+		return String.join(", ", updatedGenerics);
 	}
 	
-	private static String updateFirstInnerType(String innerType, Hashtable<String, InterfaceDefinition> replacements) {
-		return updateSingleInnerType(innerType, replacements);
+	public static String updateGeneric(String generic, Hashtable<String, InterfaceDefinition> replacements) {
+		
+		if(generic.equals    ("?"))          return generic;
+		if(generic.startsWith("? super "))   return generic;
+		if(generic.startsWith("? extends ")) return "? extends " + updateType(substringAfter(generic, "? extends "), replacements);
+		
+		String outerComponent = substringBefore(generic, "<");
+		Optional<String> replacedOuterComponent = ofNullable(replacements.get(outerComponent.trim())).map(def -> def.getName());
+		
+		if(replacedOuterComponent.isPresent()) {
+			// generic is a knowable type that can't be replaced with one of our interfaces
+			return "? extends " + updateType(generic, replacements);
+		}
+		// generic is a knowable type that can't be replaced with one of our interfaces
+		return updateType(generic, replacements);
 	}
 	
-	private static String updateRemainingInnerTypes(String innerType, Hashtable<String, InterfaceDefinition> replacements) {
-		if(StringUtils.isEmpty(innerType)) {
-			return innerType;
-		}
-		
-		StringUtils.substringBefore(innerType, "<");
-		return null;
-	}
-	
-	public static String updateOuterTypeComponent(String outerType, Hashtable<String, InterfaceDefinition> replacements) {
-		
-		checkArgument(Character.isJavaIdentifierStart(outerType.charAt(0)));
-		checkArgument(outerType.substring(1).matches("[A-Za-z0-9_$]*"));
-		if(outerType.matches(".*<.*>.*")) {
-			throw new IllegalArgumentException("Tried to perform a lookup-replacement on a Type string that included generic parameters");
-		}
-		outerType = outerType.replaceAll("[^A-Za-z0-9_$]", "");
-		Optional<String> replacementOuterType = Optional.ofNullable(replacements.get(outerType)).map(i->i.getName());
-		String newOuterType = replacementOuterType.orElse(outerType);
-		return newOuterType;
-	}
-	
-	public static String updateSingleInnerType(String singleType, Hashtable<String, InterfaceDefinition> replacements) {
-		if(singleType.startsWith("? super ")) {
-			return singleType;
-		}
-		
-		if(singleType.equals("?")) {
-			return "?";
-		}
-		
-		if(singleType.startsWith("? extends ")) {
-			String typeSubOf = StringUtils.substringAfter(singleType, "? extends ");
-			String result = "? extends " + updateOuterTypeComponent(typeSubOf, replacements);
-			log.info("updateSingleInnerType: {} ==> {}", singleType, result);
-			return result;
-		}
-		
-		String replacementType = updateOuterTypeComponent(singleType, replacements);
-		if(replacementType.equals(singleType)) {
-			return singleType;
-		}
-		return "? extends " + updateOuterTypeComponent(singleType, replacements);
-	}
-	
-	private static boolean bracketsAreBalanced(String input) {
-		return checkBalancedCharacters('<', '>', input);
+	private static boolean checkBalancedBrackets(String input) {
+		return checkBalancedCharacters('<','>',input);
 	}
 	
 	private static boolean checkBalancedCharacters(char left, char right, String input) {
