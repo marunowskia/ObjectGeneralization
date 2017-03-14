2aa301a2a833a87660d59147a9b6a69bc8125d21
Marunowski, Alexander <alexander.marunowski@one.verizon.com>
2017-03-13 19:00:48 -0700

    Added initial thoughts on collapsing a type-hierarchy into a minimal representation

--- src/main/java/com/github/marunowskia/interfacegenerator/Structure.java
+++ src/main/java/com/github/marunowskia/interfacegenerator/Structure.java
@@ -1,11 +1,11 @@
 package com.github.marunowskia.interfacegenerator;
 
 import static org.apache.commons.collections4.CollectionUtils.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
+
+import java.util.*;
+
 import com.github.marunowskia.interfacegenerator.InterfaceComposer.InterfaceDefinition;
+import lombok.NonNull;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 
@@ -23,8 +23,6 @@ public class Structure {
         // does not violate the ability for each type in "implementingTypes" to implement the recombined interface.
 
         // This occurs in two scenarios:
-
-
 
         // ========================================
         // 1) If interfaces A, B are under consideration, A,B can be merged if the following two sets are equivalent:
@@ -46,6 +44,7 @@ public class Structure {
             }
         }
         replaceAll(replacementPairs);
+        replacementPairs.clear();
 
         // ========================================
         // 2) If interfaces A, B are under consideration, then A only extends B, and A declares no additional methods
@@ -53,17 +52,56 @@ public class Structure {
         for(InterfaceDefinition definition : structureContents) {
             if(size(definition.getMustExtend()) == 1) {
                 if(isEmpty(definition.getMethodSignatures())) {
-                    replace(definition, definition.getMustExtend().get(0));
+                    replacementPairs.put(definition, definition.getMustExtend().get(0));
                 }
             }
         }
+        replaceAll(replacementPairs);
     }
 
-    public void replace(InterfaceDefinition replaceThis, InterfaceDefinition with) {
+    public void replace(InterfaceDefinition replaceThis, InterfaceDefinition with, HashMap<InterfaceDefinition, InterfaceDefinition> plannedReplacements) {
+        updateImplementors(replaceThis, with);
+        updateStructure(replaceThis, with);
+    }
+
+    private void updateStructure(InterfaceDefinition replaceThis, InterfaceDefinition with) {
+        structureContents.stream().map(id->id.getMustExtend())
+                .filter(mustExtend -> mustExtend.contains(replaceThis))
+                .forEach(mustExtend -> {
+                    mustExtend.remove(  replaceThis);
+                    mustExtend.add(     with);
+                });
+    }
+
+    private void updateImplementors(InterfaceDefinition replaceThis, InterfaceDefinition with) {
+        List<String> implementsReplaceThis = implementingTypes.getOrDefault(replaceThis, new ArrayList<>());
+        List<String> implementsWith =        implementingTypes.getOrDefault(with, new ArrayList<>());
+        implementsWith.addAll(implementsReplaceThis);
+        implementingTypes.remove(replaceThis);
+        implementingTypes.put(with, implementsWith);
 
     }
 
-    public void replaceAll(HashMap<InterfaceDefinition, InterfaceDefinition> replacementPairs) {
+    public void replaceAll(@NonNull  HashMap<InterfaceDefinition, InterfaceDefinition> plannedReplacements) {
+        while(!plannedReplacements.isEmpty()) {
+            InterfaceDefinition thisInterface = plannedReplacements.keySet().iterator().next();
+            InterfaceDefinition withThisInterface = plannedReplacements.get(thisInterface);
+            replace(thisInterface, withThisInterface, plannedReplacements);
 
+            // Update plannedReplacements so we don't re-introduce an interface we want to eliminate
+            getKeysByValue(plannedReplacements, thisInterface).forEach(key -> plannedReplacements.put(key, withThisInterface));
+            plannedReplacements.remove(thisInterface);
+        }
+    }
+
+    //http://stackoverflow.com/a/2904266
+    public static <T, E> Set<T> getKeysByValue(Map<T, E> map, E value) {
+        Set<T> keys = new HashSet<T>();
+        for (Map.Entry<T, E> entry : map.entrySet()) {
+            if (Objects.equals(value, entry.getValue())) {
+                keys.add(entry.getKey());
+            }
+        }
+        return keys;
     }
 }
