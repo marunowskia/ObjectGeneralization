a995999153f006fc6d5d21f238514da55c5d8ddb
Alexander Marunowski <marunowskia@gmail.com>
2017-03-27 19:16:18 -0700

    Added useful object for tracking both the modified return type of a method as well as the original source of that method

---
+++ src/main/java/com/github/marunowskia/interfacegenerator/GenericMethod.java
@@ -0,0 +1,53 @@
+package com.github.marunowskia.interfacegenerator;
+
+import java.util.Map;
+
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.marunowskia.interfacegenerator.InterfaceComposer.InterfaceDefinition;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class GenericMethod {
+
+	private String methodSignature;
+	private MethodDeclaration originalDeclaration;
+	
+	public void updateMethodSignature(Map<String, InterfaceDefinition> implementorMap) {
+		String updatedType = TypeUpdateUtility.updateType(originalDeclaration.getType().toStringWithoutComments(), implementorMap);
+		methodSignature = "public " + updatedType + " " + originalDeclaration.getName() + "()"; 	
+	}
+	
+	public void setOriginalDeclaration(MethodDeclaration original) {
+		this.originalDeclaration = original;
+		updateMethodSignature(null);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((methodSignature == null) ? 0 : methodSignature.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GenericMethod other = (GenericMethod) obj;
+		if (methodSignature == null) {
+			if (other.methodSignature != null)
+				return false;
+		} else if (!methodSignature.equals(other.methodSignature))
+			return false;
+		return true;
+	}
+
+	
+}
