730faad39c7d186bd65a3bf9ec3e81172aff2245
Alexander Marunowski <marunowskia@gmail.com>
2017-03-30 18:22:50 -0700

    Added missing imports required for tracking fully-qualified types

--- src/main/java/com/github/marunowskia/interfacegenerator/demo/Root.java
+++ src/main/java/com/github/marunowskia/interfacegenerator/demo/Root.java
@@ -1,7 +1,12 @@
 package com.github.marunowskia.interfacegenerator.demo;
-import com.github.marunowskia.interfacegenerator.demo.Leaf1;
+import com.github.marunowskia.interfacegenerator.demo.Leaf1; 
 import com.github.marunowskia.interfacegenerator.demo.Middle123;
+import com.github.marunowskia.interfacegenerator.demo.Middle23;
+import com.github.marunowskia.interfacegenerator.demo.Middle12;
 public class Root {
-//	public Leaf1 getSomethingNew() {return new Leaf1();}
-	public Middle123 getMiddle() {return new Middle123();}
+	public Leaf1 getSomethingNew() {return new Leaf1();}
+	
+	public Middle12  get12() {return new Middle12();}
+	public Middle123 get123() {return new Middle123();}
+	public Middle23 get23() {return new Middle23();}
 }
