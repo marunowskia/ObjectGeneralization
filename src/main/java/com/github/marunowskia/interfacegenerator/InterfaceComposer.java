e1ee93c996829b052f2a85e0d5c7f9aef0f86786
Alexander Marunowski <marunowskia@gmail.com>
2017-04-03 20:32:20 -0700

    Stabilization changes, fixing minor quirks that previously required by-hand corrections. Most of these fixes should be dealt with in a more elegant manner eventually

--- src/main/java/com/github/marunowskia/interfacegenerator/InterfaceComposer.java
+++ src/main/java/com/github/marunowskia/interfacegenerator/InterfaceComposer.java
@@ -4,22 +4,24 @@ import static java.util.Optional.ofNullable;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import javax.xml.bind.JAXBElement;
 
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.google.common.base.Charsets;
+import com.google.common.reflect.ClassPath;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.ComparatorUtils;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.base.Preconditions;
@@ -34,12 +36,52 @@ import lombok.extern.slf4j.Slf4j;
 public class InterfaceComposer {
 
 	public static void generateAndExportInterfaces(ValueGraph<String, List<GenericMethod>> methodGraph,
-			File outputDirectory) {
-		// List<InterfaceDefinition> optimizedInterfaces =
-		// InterfaceDefinitionConstraintSolver.satisfyConstraints(methodGraph);
-		// InterfaceDefinitionConstraintSolver.buildInterfaceDefinitions(methodGraph);
+												   File outputDirectory, HashMap<String,CompilationUnit> originalSources) {
 		Set<InterfaceDefinition> result = InterfaceDefinitionConstraintSolver.buildResult(methodGraph);
-		outputInterfaces(InterfaceDefinitionConstraintSolver.buildResult(methodGraph), outputDirectory);
+//		outputInterfaces(result, outputDirectory);
+		updateJavaSource(result, originalSources);
+	}
+
+	public static void updateJavaSource(Collection<InterfaceDefinition> interfaces, HashMap<String,CompilationUnit> originalSources) {
+		HashMap<String, Collection<InterfaceDefinition>> typeToMustImplementMap = new HashMap<>();
+		interfaces.forEach(iface -> {
+			iface.getImplementedBy().forEach(type -> {
+				Collection<InterfaceDefinition> typeMustImplement = typeToMustImplementMap.getOrDefault(type, new HashSet<>());
+				typeMustImplement.add(iface);
+				typeToMustImplementMap.put(type, typeMustImplement);
+			});
+		});
+
+		originalSources.forEach((name, source) -> {
+			File originalFile = (File)source.getData(); // we put this here when building "methodGraph"
+			for (TypeDeclaration typeDeclaration : source.getTypes()) {
+				if(typeDeclaration instanceof ClassOrInterfaceDeclaration) {
+
+				    if(typeToMustImplementMap.containsKey(name)) {
+                        if (((ClassOrInterfaceDeclaration) typeDeclaration).getImplements() == null) {
+                            ((ClassOrInterfaceDeclaration) typeDeclaration).setImplements(new ArrayList<>());
+                        }
+
+                        typeToMustImplementMap.get(name).forEach(mustImplement -> {
+
+                            ClassOrInterfaceType ifaceToAdd = new ClassOrInterfaceType(mustImplement.getPkg() + "." + mustImplement.getName());
+                            ((ClassOrInterfaceDeclaration) typeDeclaration).getImplements().add(ifaceToAdd);
+                        });
+                    }
+                    if(CollectionUtils.isEmpty(((ClassOrInterfaceDeclaration) typeDeclaration).getImplements())) {
+				        ((ClassOrInterfaceDeclaration) typeDeclaration).setImplements(null);
+                    }
+				}
+			}
+
+			try {
+				FileUtils.writeStringToFile(originalFile, source.toString(), Charset.defaultCharset());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+
+		});
 	}
 
 	public static void outputInterfaces(Collection<InterfaceDefinition> requiredInterfaces, File parentDirectory) {
@@ -66,6 +108,12 @@ public class InterfaceComposer {
 				outputFile.delete();
 				Files.createParentDirs(outputFile);
 				outputFile.createNewFile();
+				if(outputPath.toAbsolutePath().toString().contains("account")) {
+					if (outputPath.toAbsolutePath().toString().contains("IArrayOfExtendedAttributesObj")) {
+
+						System.out.println();
+					}
+				}
 				Files.write(javaSourceCode.getBytes(), outputFile);
 				System.out.println(javaSourceCode);
 
@@ -85,18 +133,29 @@ public class InterfaceComposer {
 		// import *.*;
 		addImports(builder, def);
 
-		//
+
 		// // Implemented by:
 		// // -> PACKAGE_1.TYPE_1
 		// // -> ... ...
 		// // -> PACKAGE_X.TYPE_X
 		addImplementedByComments(builder, def);
 
+		// // Implemented indirectly by:
+		// // -> PACKAGE_1.TYPE_1
+		// // -> ... ...
+		// // -> PACKAGE_X.TYPE_X
+		addImplementedIndirectlyByComments(builder, def);
+
 		// // Extended by:
 		// // -> PACKAGE_1.TYPE_1
 		// // -> ... ...
 		// // -> PACKAGE_Y.TYPE_Y
 		addExtendedByComments(builder, def);
+
+		// // Method signatures:
+		// // public Long getLong();
+		// // public String getString();
+		addMethodSignatureComments(builder, def);
 
 		// public interface NAME
 		// extends
@@ -159,6 +218,26 @@ public class InterfaceComposer {
 		}
 	}
 
+	private static void addImplementedIndirectlyByComments(StringBuilder builder, InterfaceDefinition def) {
+
+		Set<InterfaceDefinition> extendedBy = def.getExtendedBy();
+
+		Set<String> indirectImplementors = extendedBy.stream().map(id->id.getImplementedBy()).reduce(new HashSet<String>(), (identity, incoming) -> {
+			identity.addAll(incoming);
+			return identity;
+		});
+
+		if (!indirectImplementors.isEmpty()) {
+			builder.append("// Implemented indirectly by:\n");
+			indirectImplementors.forEach(implementor -> {
+				builder	.append("//")
+						.append(implementor).append("\n");
+			});
+
+			builder.append("\n");
+		}
+	}
+
 	private static void addExtendedByComments(StringBuilder builder, InterfaceDefinition def) {
 		Set<InterfaceDefinition> extendedBy = def.getExtendedBy();
 
@@ -178,6 +257,19 @@ public class InterfaceComposer {
 			builder.append("\n");
 		}
 
+	}
+
+	private static void addMethodSignatureComments(StringBuilder builder, InterfaceDefinition def) {
+
+		Set<GenericMethod> allMethods = def.getAllMethods();
+		if(!allMethods.isEmpty()) {
+			builder.append("// Method Signatures: \n");
+			allMethods.stream()
+					  .map(method -> "// "+method.getMethodSignature()+"\n")
+					  .sorted()
+					  .forEach(builder::append);
+
+		}
 	}
 
 	private static void addInterfaceDeclaration(StringBuilder builder, InterfaceDefinition def) {
@@ -249,8 +341,17 @@ public class InterfaceComposer {
 	}
 
 	private static void addRequiredMethods(StringBuilder builder, InterfaceDefinition def) {
-		def.methodSignatures.forEach(method -> builder.append("\tdefault ").append(method.getMethodSignature())
-				.append(" { return null;} \n\n"));
+
+		def.methodSignatures.forEach(method -> {
+				if (method.getReturnTypeString().contains("java.util.List")) {
+					builder.append("\tdefault ").append(method.getMethodSignature())
+							.append(" { return Collections.EMPTY_LIST;} \n\n");
+				} else {
+					builder.append("\tdefault ").append(method.getMethodSignature())
+							.append(" { return null;} \n\n");
+				}
+			}
+		);
 
 		if (!def.getMethodSignatures().isEmpty()) {
 			builder.append("\n");
@@ -274,7 +375,7 @@ public class InterfaceComposer {
 		// TODO: Need to work out details for automatically unwrapping various wrapper classes.
 		
 		def.getMethodSignatures().forEach(method -> {
-		
+
 			String originalMethodName = method.getOriginalDeclaration().getNameExpr().toStringWithoutComments();
 			String chainableReturnType = getChainableReturnType(method);
 			String chainableMethodBody = getChainableMethodBody(method);
@@ -301,8 +402,28 @@ public class InterfaceComposer {
 
 	private static String getChainableMethodBody(GenericMethod method) {
 		String returnType = getChainableReturnType(method);
+
+
 		if(method.getReturnTypeString().contains("JAXBElement")) {
-			return "\t\tOptional< ? extends " + returnType + "> result = ComOp.oj(" + getOriginalMethodName(method) + "());\n\t\treturn result.isPresent()?result.get() : " + returnType + ".missing();"; 
+		    boolean shouldReturnMissing = true;
+
+		    try {
+                Collection<ClassPath.ClassInfo> javaLangClasses = ClassPath.from(ClassLoader.getSystemClassLoader()).getTopLevelClasses("java.lang");
+                if(javaLangClasses.stream().anyMatch(info -> info.getSimpleName().equals(returnType))) {
+                    shouldReturnMissing = false;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+			if(shouldReturnMissing) {
+				return "\t\tOptional< ? extends " + returnType + "> result = ComOp.oj(" + getOriginalMethodName(method)
+						+ "());\n\t\treturn result.isPresent()?result.get() : " + returnType + ".missing();";
+			}
+			return "\t\tOptional< ? extends " + returnType + "> result = ComOp.oj(" + getOriginalMethodName(method)
+					+ "());\n\t\treturn result.isPresent()?result.get() : null;";
 		}
 		return "\t\treturn " + getOriginalMethodName(method) + "();";
 	}
@@ -315,13 +436,15 @@ public class InterfaceComposer {
 		}
 		return method.getReturnTypeString();
 	}
-private static void addIsMissingComponent(StringBuilder builder, InterfaceDefinition def) {
+
+	private static void addIsMissingComponent(StringBuilder builder, InterfaceDefinition def) {
 		builder.append("\tpublic static final ").append(def.getName()).append(" MISSING = new ").append(def.getName())
 				.append("() {};\n");
 
 		builder.append("\tpublic static ").append(def.getName()).append(" missing() { return MISSING;}\n");
 
 		builder.append("\tpublic default boolean isMissing() {return this==MISSING;}\n\n");
+		builder.append("\tpublic default boolean isPresent() {return this!=MISSING;}\n\n");
 	}
 
 	@Data
@@ -343,6 +466,26 @@ private static void addIsMissingComponent(StringBuilder builder, InterfaceDefini
 			this.pkg = pkg;
 			this.name = name;
 		}
+
+		public Set<GenericMethod> getAllMethods() {
+			System.out.println("new getAllMethods request: " + pkg + "." + name );
+
+			return getAllMethods(0);
+		}
+		private Set<GenericMethod> getAllMethods(int depth) {
+			// Will break on cyclic dependencies. But so will Java itself...
+			Set<GenericMethod> result = new HashSet<GenericMethod>();
+			result.addAll(getMethodSignatures());
+
+			getMustExtend().forEach(extended->result.addAll(extended.getAllMethods( depth+1)));
+
+			if(getMustExtend().isEmpty()) {
+//				System.out.println(depth);
+			}
+			return result;
+		}
+
+
 
 		public InterfaceDefinition() {
 		}
